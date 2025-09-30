f89ae073fb797e1dbacfd3ded85956168bb38052
Victor Gm <bitxogm1976@gmail.com>
2025-09-30 07:44:32 +0200

    fix: Solucionado error API Key de Firebase Auth y completada refactorizacion Python

--- functions/src/services/geminiService.ts
+++ functions/src/services/geminiService.ts
@@ -19,7 +19,7 @@ export async function getGeminiAnalysis(
   }
 
   const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+  const model = genAI.getGenerativeModel({model: "gemini-2.5-flash"});
 
   let instruction = "";
 
